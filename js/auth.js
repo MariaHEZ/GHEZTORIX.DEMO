26b3ada298e4516b8d25a860e63d798207a5cfab
MariaHEZ <jaque.dez03@gmail.com>
2026-06-21 22:03:23 -0600

    Refactor login function to handle user roles

--- js/auth.js
+++ js/auth.js
@@ -1,17 +1,24 @@
 function login() {
+  const perfil = document.getElementById("perfil").value;
   const usuario = document.getElementById("usuario").value.trim();
   const password = document.getElementById("password").value.trim();
 
   const adminUser = "admin@correo.com";
   const adminPass = "12345";
 
-  if (usuario === adminUser && password === adminPass) {
-    alert("Bienvenido Administrador");
-    window.location.href = "dashboard.html";
-  } else if (usuario.length > 0 && password.length > 0) {
-    alert("Acceso como Cliente");
-    window.location.href = "cliente.html";
-  } else {
-    alert("Por favor ingresa tus datos");
+  if (perfil === "usuario") {
+    if (usuario === adminUser && password === adminPass) {
+      alert("Bienvenido Administrador");
+      window.location.href = "dashboard.html";
+    } else {
+      alert("Credenciales inválidas para Usuario");
+    }
+  } else if (perfil === "cliente") {
+    if (usuario.length > 0 && password.length > 0) {
+      alert("Bienvenido Cliente");
+      window.location.href = "cliente.html";
+    } else {
+      alert("Por favor ingresa tus datos");
+    }
   }
 }
