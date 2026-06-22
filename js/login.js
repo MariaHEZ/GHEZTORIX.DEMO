2cbb3aa0e2186c4bd718d17185620bafaa251846
MariaHEZ <jaque.dez03@gmail.com>
2026-06-22 09:33:29 -0600

    Update login.js

--- js/login.js
+++ js/login.js
@@ -3,17 +3,24 @@ function login(){
     const usuario = document.getElementById("usuario").value;
     const password = document.getElementById("password").value;
 
-    if(
-        (usuario === "admin" && password === "123456") ||
-        (usuario === "cliente1" && password === "123456")
-    ){
+    // usuarios simulados
+    const usuarios = [
+        {usuario:"admin", password:"123456", rol:"admin"},
+        {usuario:"cliente1", password:"123456", rol:"cliente"}
+    ];
 
-        localStorage.setItem("sesion","ok");
+    const acceso = usuarios.find(u =>
+        u.usuario === usuario && u.password === password
+    );
+
+    if(acceso){
+
+        // guardamos usuario completo
+        localStorage.setItem("sesion", JSON.stringify(acceso));
 
         window.location.href = "dashboard.html";
 
     }else{
-
         document.getElementById("error").innerText =
         "Usuario o contraseña incorrectos";
     }
