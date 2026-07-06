e7201d17e936e1d11a0e53c5deda55fedcf45338
MariaHEZ <jaque.dez03@gmail.com>
2026-07-06 16:24:33 -0600

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,1 +1,50 @@
+// Cargar estadísticas del dashboard
 
+let clientes = JSON.parse(localStorage.getItem("clientes")) || [];
+
+
+// Total clientes
+
+document.getElementById("totalClientes").innerHTML =
+clientes.length;
+
+
+
+// Últimos clientes registrados
+
+let contenedor =
+document.getElementById("ultimosClientes");
+
+
+if(clientes.length > 0){
+
+
+contenedor.innerHTML="";
+
+
+clientes.slice(-3).reverse().forEach(cliente=>{
+
+
+contenedor.innerHTML += `
+
+<div class="cliente-mini">
+
+<h3>
+${cliente.razon || cliente.nombre}
+</h3>
+
+
+<p>
+RFC: ${cliente.rfc}
+</p>
+
+
+</div>
+
+`;
+
+
+});
+
+
+}
