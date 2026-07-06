009e9de82f2209ef9bfc977bd612a76f2eebf81b
MariaHEZ <jaque.dez03@gmail.com>
2026-07-06 11:40:41 -0600

    Retrieve and log clients from localStorage
    Added functionality to retrieve clients from localStorage and log them.

--- js/clientes.js
+++ js/clientes.js
@@ -1,1 +1,6 @@
 console.log("clientes.js cargado");
+// Obtener clientes guardados o crear lista vacía
+let clientes = JSON.parse(localStorage.getItem("clientes")) || [];
+
+// Mostrar clientes en consola
+console.log("Clientes cargados:", clientes);
