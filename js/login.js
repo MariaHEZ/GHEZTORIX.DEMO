be7f40b7d509d61ca7042e9078929314ffd5da8d
MariaHEZ <jaque.dez03@gmail.com>
2026-07-06 19:43:40 -0600

    Refactor login function to handle admin and client access

--- js/login.js
+++ js/login.js
@@ -1,7 +1,217 @@
 function loginAdmin(){
 
 
+let usuario =alet tipoAcceso="";
+
+
+function mostrarLogin(tipo){
+
+
+tipoAcceso = tipo;
+
+
+let formulario =
+document.getElementById("loginForm");
+
+
+
+if(tipo==="admin"){
+
+
+formulario.innerHTML = `
+
+
+<h3>
+Acceso Administrador
+</h3>
+
+
+<input 
+id="usuario"
+placeholder="Usuario">
+
+
+<input
+id="password"
+type="password"
+placeholder="Contraseña">
+
+
+<button onclick="ingresar()">
+
+Ingresar
+
+</button>
+
+
+`;
+
+
+}
+
+
+
+if(tipo==="cliente"){
+
+
+formulario.innerHTML = `
+
+
+<h3>
+Acceso Cliente
+</h3>
+
+
+<input
+id="rfc"
+placeholder="RFC">
+
+
+<input
+id="password"
+type="password"
+placeholder="Contraseña">
+
+
+<button onclick="ingresar()">
+
+Ingresar
+
+</button>
+
+
+`;
+
+
+}
+
+
+}
+
+
+
+function ingresar(){
+
+
+
+if(tipoAcceso==="admin"){
+
+
 let usuario =
+document.getElementById("usuario").value;
+
+
+let password =
+document.getElementById("password").value;
+
+
+
+if(
+usuario==="admin"
+&&
+password==="12345"
+
+){
+
+
+localStorage.setItem(
+"rol",
+"admin"
+);
+
+
+
+location.href=
+"dashboard-admin.html";
+
+
+}
+
+else{
+
+
+alert(
+"Datos incorrectos"
+);
+
+
+}
+
+
+}
+
+
+
+
+if(tipoAcceso==="cliente"){
+
+
+let rfc =
+document.getElementById("rfc").value;
+
+
+let password =
+document.getElementById("password").value;
+
+
+
+let clientes =
+JSON.parse(
+localStorage.getItem("clientes")
+)
+|| [];
+
+
+
+let cliente =
+clientes.find(c=>
+
+c.rfc===rfc
+&&
+c.password===password
+
+);
+
+
+
+if(cliente){
+
+
+localStorage.setItem(
+"clienteActivo",
+JSON.stringify(cliente)
+);
+
+
+
+localStorage.setItem(
+"rol",
+"cliente"
+);
+
+
+
+location.href=
+"dashboard-cliente.html";
+
+
+}
+
+else{
+
+
+alert(
+"Cliente no encontrado"
+);
+
+
+}
+
+
+}
+
+
+}
 document.getElementById("usuario").value;
 
 
