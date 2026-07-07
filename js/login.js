72d914576643779e26a1793d3ffcd129833dad50
MariaHEZ <jaque.dez03@gmail.com>
2026-07-06 21:02:39 -0600

    Fix redirection comments in login.js

--- js/login.js
+++ js/login.js
@@ -1,14 +1,14 @@
 if (tipoPortal === "admin") {
   acceso = usuariosAdmin.some(u => u.correo === usuario && u.password === password);
   if (acceso) {
-    window.location.href = "dashboard_admin.html"; // redirección al dashboard de admin
+    window.location.href = "dashboard_admin.html";
   } else {
     mensaje.textContent = "Correo o contraseña incorrectos ❌";
   }
 } else if (tipoPortal === "cliente") {
   acceso = usuariosCliente.some(u => u.rfc === usuario && u.password === password);
   if (acceso) {
-    window.location.href = "dashboard_cliente.html"; // redirección al dashboard de cliente
+    window.location.href = "dashboard_cliente.html";
   } else {
     mensaje.textContent = "RFC o contraseña incorrectos ❌";
   }
